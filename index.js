e96edb7ae75e6683603abb00f2abba524f12d814
Thananan Worrawongvutikrai <bluebearrii@Thananans-MacBook-Pro.local>
2021-04-27 17:41:06 +0700

    :zap:[API] test real-time snap shot

--- index.js
+++ index.js
@@ -11,128 +11,15 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-app.post("/api/get-trips", (req, res) => {
-  const docRef = db.collection("trips");
-  docRef
-    .where("uid", "==", req.headers.uid)
-    .get()
-    .then((snapshot) => {
-      var arr = [];
-      snapshot.forEach((doc) => {
-        arr.push(doc.data());
-      });
-
-      return res.status(200).json(arr);
-    }).catch(err => {
-      return res.status(500).json(err);
-    })
-});
-
-app.post("/api/create-trip", (req, res) => {
-  const body = {
-    uid: req.body.uid,
-    tripId: null,
-    name: req.body.name,
-    budget: req.body.budget,
-    date: req.body.date,
-    rating: null,
-    wallpaper: req.body.wallpaper,
-    storyTelling: null,
-  };
-  console.log(body);
-
-  const docRef = db.collection("trips").add(body);
-  docRef
-    .then((serverResponse) => {
-      serverResponse
-        .update({ tripId: serverResponse.id })
-        .then((updateResponse) => {
-          return res.status(201).json({
-            message: "Create new trip successful",
-            serverResponse,
-            updateResponse,
-          });
-        })
-        .catch((updateErr) => {
-          return res.status(500).json({
-            message: "Something wrong can not update trip id",
-            updateErr,
-          });
-        });
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: "Something wrong can not create new trip", err });
+app.get("/api/get-update", (req, res) => {
+  db.collection("trips").onSnapshot((querySnapshot) => {
+    var trips = [];
+    querySnapshot.docs.forEach((change) => {
+      trips.push(change.id);
     });
-});
-
-app.post("/api/booking", (req, res) => {
-  const body = {
-    tripId: req.body.tripId,
-    uid: req.body.uid,
-    date: req.body.date,
-    time: req.body.time,
-    placeId: req.body.placeId,
-    type: req.body.type,
-    status: "pending",
-  };
-
-  console.log(body);
-
-  const docRef = db.collection("bookings").add(body);
-  docRef
-    .then((serverResponse) => {
-      return res.status(201).json({
-        message: "Booking successfull",
-        serverResponse,
-      });
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: "Something wrong, booking not success", err });
-    });
-});
-
-app.post("/api/hosting/register", (req, res) => {
-  const body = {
-    uid: req.body.uid,
-    placeId: null,
-    title: req.body.title,
-    location: req.body.location,
-    type: req.body.type,
-    rule: req.body.rule,
-    pricing: req.body.pricing,
-    review: null,
-    rate: null,
-  };
-  console.log(body);
-
-  const docRef = db.collection("hosting").add(body);
-  docRef
-    .then((serverResponse) => {
-      serverResponse
-        .update({ placeId: serverResponse.id })
-        .then((updateResponse) => {
-          return res.status(201).json({
-            message: "Hosting successfull",
-            serverResponse,
-            updateResponse,
-          });
-        })
-        .catch((updateErr) => {
-          return res.status(500).json({
-            message: "Something wrong can not update host id",
-            updateErr,
-          });
-        });
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: "Something wrong, hosting not success", err });
-    });
+    console.log(trips);
+    return res.status(200).json({trips});
+  });
 });
 
 const PORT = process.env.PORT || 5000;
