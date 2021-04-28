cf2efb6e1759a3ae2ea53f9d5b91ffa4644fbdd5
Thananan Worrawongvutikrai <bluebearrii@Thananans-MacBook-Pro.local>
2021-04-28 14:39:47 +0700

    :wrench: [Controller] Create trip & upload image

--- controller/trip.js
+++ controller/trip.js
@@ -1,48 +1,46 @@
 const express = require("express");
 const fs = require("fs");
 const admin = require("firebase-admin");
+const { url } = require("inspector");
+const { format } = require("path");
 
 const db = admin.firestore();
 const storage = admin.storage().bucket();
 
 exports.createTrip = async (req, res) => {
-  //const { file } = req.body;
-
-  console.log(req.file);
-  // try {
-  //     const addTrip = await db.collection("file").add(req.body);
-  //     res.status(200).json(addTrip);
-  // } catch (error) {
-  //     res.status(500).json(error);
-  // }
+  const { file } = req;
+  const {title, budget, tripId, uid} = req.body
+  const filename = `${tripId}_${uid}`
+  const url = `https://firebasestorage.googleapis.com/v0/b/${storage.name}/o/${filename}?alt=media`;
   try {
-    const file = await storage.file(`${req.file.originalname}`);
-    const blobStream = file.createWriteStream({
+    const _file = await storage.file(filename);
+
+    const blobStream = await _file.createWriteStream({
       metadata: {
-        contentType: req.file.mimetype,
+        contentType: file.mimetype,
+        firebaseStorageDownloadTokens: null,
       },
     });
+
     blobStream.on("error", (error) => {
       console.log(error);
     });
+
     blobStream.on("finish", async () => {
       console.log("Upload Done");
-      const url = `https://firebasestorage.googleapis.com/v0/b/${storage.name}/o/${req.file.originalname}?alt=media`;
-      res.status(200).json({ url: url });
+      const addTrip = await db.collection("file").add({title: title,budget: budget, path: url});
+      res.status(200).json(addTrip);
     });
-    blobStream.end(req.file.buffer);
-
-
-    // const file = await storage
-    //   .file(req.file.originalname, {
-    //     firebaseStorageDownloadTokens: null,
-    //   })
-    //   .save(req.file.buffer);
-
-    // console.log(file);
-    // const url = `https://firebasestorage.googleapis.com/v0/b/${storage.name}/o/${req.file.originalname}?alt=media`;
-    // res.status(200).json({ url: url });
+    blobStream.end(file.buffer);
   } catch (error) {
-    res.status(404).json(error);
+    res.status(500).json(error);
   }
+
+  // console.log(req.file);
+  // try {
+  //      const addTrip = await db.collection("file").add(req.body);
+  //      res.status(200).json(addTrip);
+  //  } catch (error) {
+  //      res.status(500).json(error);
+  //  }
 };
