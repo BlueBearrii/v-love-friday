b48a27995b72c878fdbbbb008e828d9c51c94435
Thananan Worrawongvutikrai <thananan.math.acs@gmail.com>
2021-05-03 13:51:31 +0700

    :zap: [Controller] Update images upload function

--- controller/trip.js
+++ controller/trip.js
@@ -9,13 +9,12 @@ const storage = admin.storage().bucket();
 
 exports.createTrip = async (req, res) => {
   const { file } = req;
-  const {title, budget, tripId, uid} = req.body
-  const filename = `${tripId}_${uid}`
-  const url = `https://firebasestorage.googleapis.com/v0/b/${storage.name}/o/${filename}?alt=media`;
+  const { title, budget, tripId, uid, status } = req.body;
+  const filename = `${tripId}_${uid}`;
+  const url = `https://firebasestorage.googleapis.com/v0/b/${storage.name}/o/coverImages%2F${filename}?alt=media`;
 
-  
   try {
-    const _file = await storage.file(filename);
+    const _file = await storage.file(`coverImages/${filename}`);
 
     const blobStream = await _file.createWriteStream({
       metadata: {
@@ -30,19 +29,56 @@ exports.createTrip = async (req, res) => {
 
     blobStream.on("finish", async () => {
       console.log("Upload Done");
-      const addTrip = await db.collection("file").add({title: title,budget: budget, path: url});
+      const addTrip = await db.collection("trips").add({
+        title: title,
+        budget: budget,
+        path: url,
+        tripId : tripId,
+        uid: uid,
+        status: status,
+      });
       res.status(200).json(addTrip);
     });
     blobStream.end(file.buffer);
   } catch (error) {
     res.status(500).json(error);
   }
+};
 
-  // console.log(req.file);
-  // try {
-  //      const addTrip = await db.collection("file").add(req.body);
-  //      res.status(200).json(addTrip);
-  //  } catch (error) {
-  //      res.status(500).json(error);
-  //  }
+exports.getTrips = async (req, res) => {
+  const { uid } = req.body;
+  var array = [];
+
+  try {
+    const fetchTrips = await db.collection("trips").get();
+
+    fetchTrips.docs.forEach((doc) => {
+      if (doc.data().uid == uid) array.push(doc.data());
+      else console.log("not done");
+    });
+
+    res.status(200).json(array);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.deleteTrip = async (req, res) => {
+  const { uid, tripId } = req.body;
+  const filename = `${tripId}_${uid}`;
+  try {
+    const ref = await db.collection("trips");
+    const fetchTrips = await ref.get();
+    fetchTrips.docs.forEach(async (doc) => {
+      if (doc.data().uid == uid && doc.data().tripId == tripId) {
+        ref.doc(doc.id).delete();
+        await storage.file(`coverImages/${tripId}_${uid}`).delete()
+
+      } else console.log("not done");
+    });
+
+    res.status(200).json({ status: "done" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
--- routes/api/trip.js
+++ routes/api/trip.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const multer = require("../../utils/multerUtil");
 
-const {createTrip} = require("../../controller/trip");
+const {createTrip, getTrips, deleteTrip} = require("../../controller/trip");
 
 router.route('/createTrip').post(multer.single("file"), createTrip);
-
+router.route('/getTrips').post(getTrips);
+router.route('/deleteTrip').delete(deleteTrip);
 module.exports = router;
