cf1613cf913313ae977e56a6864f1a8108aa418a
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-05-29 19:21:18 +0700

    :sparkles:  [API] Create trip

--- controller/trip.js
+++ controller/trip.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const admin = require("firebase-admin");
+
+const firestore = admin.firestore();
 
 exports.createTripRoom = async (req, res) => {
-  
+  const { uid, tripName, budget, days, place } = req.body;
+  const _trip = {
+    uid: uid,
+    tripName: tripName,
+    budget: budget,
+    balance: budget,
+    days: days,
+    place: place
+  }
+
+  try {
+    const _createTripRoom = await firestore.collection("trips").add(_trip)
+
+    const update = await _createTripRoom.update({ tripId: _createTripRoom.id })
+
+    res.status(201).json({ code: "trip/created", message: update })
+  } catch (error) {
+    res.status(400).json(error)
+  }
+
 }
