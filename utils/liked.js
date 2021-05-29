ebd4c8154d83977f977dff35b9ac071fd930c515
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-05-29 17:14:00 +0700

    :sparkles:  [Utils] Dynamic like function

---
+++ utils/liked.js
@@ -0,0 +1,33 @@
+const admin = require("firebase-admin");
+
+const firestore = admin.firestore();
+
+module.exports = async (collection, id, uid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const ref = await firestore.collection(collection).doc(id)
+
+            const getExist = await (await ref.get()).data().likes
+ 
+            const isExist = await getExist.includes(uid)
+
+            if (isExist) {
+                const _romoveLike = await ref.update({
+                    likes: admin.firestore.FieldValue.arrayRemove(uid)
+                })
+
+                resolve(_romoveLike);
+            } else {
+                const _updateLike = await ref.update({
+                    likes: admin.firestore.FieldValue.arrayUnion(uid)
+                })
+
+                resolve(_updateLike);
+            }
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
