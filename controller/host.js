6ab3844e2cbdb9800f9af8e55a3a6850331d0332
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-05-29 17:13:32 +0700

    :+1:  [Update] Like host

--- controller/host.js
+++ controller/host.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const admin = require("firebase-admin");
+const liked = require("../utils/liked");
 
-const auth = admin.auth();
 const firestore = admin.firestore();
 
 exports.createHost = async (req, res) => {
     const { lifstyle, hostname, address, contact, uid } = req.body;
+    const host = {
+        lifstyle: lifstyle,
+        hostname: hostname,
+        address: address,
+        contact: contact,
+        uid: uid,
+        likes: [],
+        images: []
+    }
 
     try {
 
-        res.json({ code: "auth/user-found", message: "hello" });
+        const createHost = await firestore.collection("hosting").add(host);
+
+        const update = await createHost.update({ hostid: createHost.id })
+
+        res.status(201).json({ code: "host/created", message: update })
 
     } catch (error) {
-        res.json(error)
+        res.status(400).json(error)
+    }
+}
+
+exports.likeHost = async (req, res) => {
+    const { id, uid } = req.body;
+    const _collection = "hosting"
+    try {
+
+        const like = await liked(_collection, id, uid);
+
+        res.status(201).json({ code: "host/liked", message: like })
+
+    } catch (error) {
+        res.status(400).json(error)
     }
 }
 
--- routes/api/host.js
+++ routes/api/host.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { verifiedUid } = require('../../middleware/verifiedUid');
 
 
-const { createHost } = require("../../controller/host");
+const { createHost, likeHost } = require("../../controller/host");
 
 router.route('/createHost').post(verifiedUid, createHost);
+router.route('/likeHost').post(verifiedUid, likeHost);
 
 module.exports = router;
