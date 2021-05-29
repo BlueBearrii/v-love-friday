03cb896424c6f4089370f10b7f716f60a24bdf2c
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-05-29 17:14:44 +0700

    :sparkles:  [API] Create trip route

--- controller/trip.js
+++ controller/trip.js
@@ -1,14 +1,5 @@
 const express = require("express");
-const upload_image = require("../utils/upload_image");
 
-exports.testUpload = async (req, res) => {
-  const { file } = req;
-  const { path, name } = req.body;
-
-  try {
-    const upload = await upload_image(file, path, name);
-    res.status(200).json({message : upload})
-  } catch (error) {
-    res.status(500).json({status : "error"})
-  }
+exports.createTripRoom = async (req, res) => {
+  
 }
--- routes/api/trip.js
+++ routes/api/trip.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("../../utils/multerUtil");
+const { verifiedUid } = require('../../middleware/verifiedUid');
 
-const { testUpload } = require("../../controller/trip");
 
-router.route('/testUpload').post(multer.single("file"), testUpload);
+const { createTripRoom } = require("../../controller/trip");
+
+router.route('/createTripRoom').post(verifiedUid, createTripRoom);
 
 module.exports = router;
