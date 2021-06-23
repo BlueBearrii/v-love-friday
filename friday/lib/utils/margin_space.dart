13aaa06d0e74819f26e270f2ab9461b1b15e5f40
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-06-23 20:13:11 +0700

    [Utils]

---
+++ friday/lib/utils/margin_space.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget marginSpace(double y_value, double x_value) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: y_value, horizontal: x_value),
+  );
+}
