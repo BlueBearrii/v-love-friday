718de46898e1a60cd249614d77a80bf6420d2c5a
THANANAN-DEV <thananan.math.acs@gmail.com>
2021-06-29 03:22:04 +0700

    :fire:

--- friday/lib/utils/margin_space.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget marginSpace(double y_value, double x_value) {
-  return Container(
-    margin: EdgeInsets.symmetric(vertical: y_value, horizontal: x_value),
-  );
-}
--- utils/firebaseInitializeApp.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../config/serviceAccountKey.json");
-
-module.exports = () => {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        storageBucket : "gs://vr-love-friday.appspot.com"
-      });
-}
